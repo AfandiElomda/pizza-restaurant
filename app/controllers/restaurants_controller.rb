204c76afaea8b5aab8c91482fb5f903b5052c860
+ <+>
2022-12-04 02:04:39 +0300

    updated migrations

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,27 @@
 class RestaurantsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
+    def index
+        render json: Restaurant.all
+    end
+
+    def show
+        restaurant = find_restaurant
+        render json: restaurant, serializer: RestaurantHavingPizzasSerializer
+    end
+
+    def destroy
+        restaurant = find_restaurant.destroy
+        head :no_content
+        
+    end
+
+    private
+
+    def record_not_found_response(exception)
+     render json: { error:"Restaurant not found"}, status: :not_found
+    end
+
+    def find_restaurant
+        Restaurant.find(params[:id])
+    end
 end
