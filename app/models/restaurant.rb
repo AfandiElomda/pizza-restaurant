c0a61046ecd557e9a747005de7570314b44ce3bc
+ <+>
2022-12-04 12:45:50 +0300

    questions && comments

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,8 @@
 class Restaurant < ApplicationRecord
+   # I've tried the seed replant after i deleted data from my database,how can regenerate deleted data??
+   #could you give me feedback, thankyou!!
+
     has_many :restaurant_pizzas, dependent: :destroy
     has_many :pizzas , through: :restaurant_pizzas
+
 end
